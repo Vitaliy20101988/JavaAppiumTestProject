10f90b00ae4d1d8c55317a13abeb7466e2861176
Виталий Епанча <vitaliy@Vitalijs-MacBook-Pro.local>
2021-02-25 01:17:55 +0200

    Lesson 1 task 3

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,7 @@
 public class MainClass {
 
     private int class_number = 20;
+    private String class_string = "Hello, world";
 
     public int getLocalNumber() {
         return 14;
@@ -8,5 +9,9 @@ public class MainClass {
 
     public int getClass_number() {
         return class_number;
+    }
+
+    public String getClass_string() {
+        return class_string;
     }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -4,19 +4,29 @@ import org.junit.Test;
 public class MainClassTest {
 
     MainClass mainClass = new MainClass();
+    String errorMessage;
 
     @Test
     public void testGetLocalNumber() {
         int actualResult = mainClass.getLocalNumber();
         int expectedResult = 14;
-        String errorMessage = "Variable have to be: " + expectedResult + " but actual variable is: " + actualResult;
+        errorMessage = "Variable have to be: " + expectedResult + " but actual variable is: " + actualResult;
         Assert.assertEquals(errorMessage, expectedResult, actualResult);
     }
 
     @Test
     public void testGetClassNumber() {
-        String errorMessage = "Variable have to be more than 45.";
+        errorMessage = "Variable have to be more than 45.";
         Assert.assertTrue(errorMessage, mainClass.getClass_number() > 45);
+    }
+
+    @Test
+    public void testGetClassString() {
+        String hello = "hello";
+        String hello_1 = "Hello";
+        boolean expectedResult = mainClass.getClass_string().contains(hello) || mainClass.getClass_string().contains(hello_1);
+        errorMessage = "Text have to be contains " + hello + " or " + hello_1;
+        Assert.assertTrue(errorMessage, expectedResult);
     }
 
 }
